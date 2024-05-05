c2d1c3016776563e1eff44504ae611309b0eea06
CJ Way <cjway1987@gmail.com>
2024-05-05 22:04:13 +0000

    add create and show for /finstagram_posts

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,11 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
-    @finstagram_posts = FinstagramPost.order(created_at: :desc)    
+    @finstagram_posts = FinstagramPost.order(created_at: :desc)  
     erb(:index)
 
 end
@@ -18,8 +24,56 @@ post '/signup' do
     @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
   
     if @user.save
-      "User #{username} saved!"
+      redirect to ('/login')
     else
       erb(:signup)
     end
   end
+ get '/login' do    # when a GET request comes into /login
+   erb(:login)      # render app/views/login.erb
+ end
+
+post '/login' do
+   username = params[:username]
+   password = params[:password]
+ 
+   # 1. find user by username
+   @user = User.find_by(username: username)
+ 
+   # 2. if that user exists and if the passwords match
+   if @user && @user.password == password
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      @error_message = "Login Failed."
+      erb(:login)
+   end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
+end
+
+get '/finstagram_posts/new' do
+  @finstagram_post = FinstagramPost.new
+  erb(:"finstagram_posts/new")
+end
+
+post '/finstagram_posts' do
+  photo_url = params[:photo_url]
+
+  @finstagram_post = FinstagramPost.new({ photo_url: photo_url, user_id: current_user.id })
+
+  if @finstagram_post.save
+    redirect to('/')
+  else
+    erb(:"finstagram_posts/new")
+  end
+end
+
+get '/finstagram_posts/:id' do
+  @finstagram_post = FinstagramPost.find(params[:id])
+  erb(:"finstagram_posts/show")
+end
+
